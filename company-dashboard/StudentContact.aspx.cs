03aa7665faa7426468851094a6ebfdd3eb8f96e2
William Dalton <dalto2wk@dukes.jmu.edu>
2019-03-28 19:55:14 -0700

    Inserted test pdf's into db. Added helper c# methods to do so

--- company-dashboard/StudentContact.aspx.cs
+++ company-dashboard/StudentContact.aspx.cs
@@ -19,7 +19,9 @@ public partial class company_dashboard_StudentContact : System.Web.UI.Page
     {
         int buttonRowIndex = Convert.ToInt32(e.CommandArgument);
         GridViewRow row = studentApplicationTable.Rows[buttonRowIndex];
-        pdfProcessToDB();
+
+        //calls method for converting test data pdf into binary and inserting to db
+        //pdfProcessToDB();
     }
     protected void pdfProcessToDB()
     {
@@ -29,14 +31,41 @@ public partial class company_dashboard_StudentContact : System.Web.UI.Page
         //file:///C:/Users/WK/Downloads/William%20Dalton%20Resume%202018.pdf
         //Add a pdf file in the below code to add to database
         //C:\\Users\\WK\\Downloads\\ProjectResumesSamples\\Jon Snow.pdf
-        FileStream fStream = File.OpenRead("C:\\Users\\WK\\Downloads\\ProjectResumesSamples\\Jon Snow.pdf");
+        FileStream fStream = File.OpenRead("C:\\Users\\WK\\Downloads\\ProjectResumesSamples\\RobertStark.pdf");
         byte[] contents = new byte[fStream.Length];
         fStream.Read(contents, 0, (int)fStream.Length);
         fStream.Close();
 
-        System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("Update Application set resume = (@data) where studentID = 3", cn);
+        System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("Update Application set resume = (@data) where studentID = 6", cn);
         cmd.Parameters.AddWithValue("@Data", contents);
         cmd.ExecuteNonQuery();
         cn.Close();
+    }
+    protected void pdfWrite()
+    {
+        //string savedFilePath = Server.MapPath("~\\Files\\Report.pdf");
+        System.Data.SqlClient.SqlConnection cn = new System.Data.SqlClient.SqlConnection("Data Source=localhost;Initial Catalog=PDFTest;Integrated Security=True");
+        cn.Open();
+
+        System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand("select pdf from savePDF where ID= 1", cn);
+
+
+        System.Data.SqlClient.SqlDataReader dr = cmd.ExecuteReader(System.Data.CommandBehavior.Default);
+
+        if (dr.Read())
+        {
+            byte[] fileData = (byte[])dr.GetValue(0);
+            //System.IO.FileStream fs = new System.IO.FileStream(savedFilePath, System.IO.FileMode.Create, System.IO.FileAccess.ReadWrite);
+
+            //System.IO.BinaryWriter bw = new System.IO.BinaryWriter(fs);
+            //Response.BinaryWrite(fileData);
+            //bw.Write(fileData);
+            //bw.Close();
+        }
+
+        dr.Close();
+        //the below way stores to solution using response.binarywrite is better
+        //Response.Redirect("~\\Files\\Report.pdf");
+
     }
 }
