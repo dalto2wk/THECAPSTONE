37c9a20556a5b23caac5f054d10dcbe83b5a9716
Kyle Mangine <manginkm@dukes.jmu.edu>
2019-04-10 16:38:40 -0400

    worked on top candidate a little more

--- company-dashboard/StudentContact.aspx.cs
+++ company-dashboard/StudentContact.aspx.cs
@@ -133,17 +133,18 @@ public partial class company_dashboard_StudentContact : System.Web.UI.Page
 
     public void TopCandidate()
     {
+
+        ///////////////////////////////////Top Candidate Code////////////////////////////////////////////
+        ///Declare and Initialize lists
+        List<TopCandidate> topCandidate = new List<TopCandidate>();
+        ArrayList postingInterest = new ArrayList();
+
+
+        ///Connect to database
+        System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["AWSString"].ConnectionString);
+
         try
         {
-            ///////////////////////////////////Top Candidate Code////////////////////////////////////////////
-            ///Declare and Initialize lists
-            List<TopCandidate> topCandidate = new List<TopCandidate>();
-            ArrayList postingInterest = new ArrayList();
-
-
-            ///Connect to database
-            System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["AWSString"].ConnectionString);
-
             ///Grab the posting interests from the posting
             sc.Open();
             System.Data.SqlClient.SqlCommand getPostingInterest = new System.Data.SqlClient.SqlCommand
@@ -163,56 +164,79 @@ public partial class company_dashboard_StudentContact : System.Web.UI.Page
                 postingInterest.Add(reader2.GetInt32(1));
             }
             sc.Close();
+        }
+        catch
+        {
+            postingInterest.Add(0);
+        }
+
+        sc.Open();
+        System.Data.SqlClient.SqlCommand getTopCandidate = new System.Data.SqlClient.SqlCommand
+
+        {
+            ///select the firstname, lastname, GPA, and interest name from all the students who applied to
+            ///a particular job posting
+            Connection = sc,
+            CommandText = "SELECT Student.StudentID, Student.FirstName, Student.LastName, Student.GPA, Student_Interest.interestID " +
+            "FROM Student INNER JOIN Student_Interest ON Student.StudentID = Student_Interest.studentID " +
+            "INNER JOIN Application ON Student.StudentID = Application.studentID INNER JOIN " +
+            "Posting ON Application.postingID = Posting.postingID where Posting.PostingID = @ddlTopCandidate"
+        };
+        getTopCandidate.Parameters.AddWithValue("@ddlTopCandidate", ddlTopCandidate.SelectedValue);
+
+        //create an array list to store the names, GPAs, and interests all separately
+        SqlDataReader reader = getTopCandidate.ExecuteReader();
+
+        int id = 0;
+        String firstName = "";
+        String lastName = "";
+        double GPA = 0;
+        int studentInterestID = 0;
 
 
-            sc.Open();
-            System.Data.SqlClient.SqlCommand getTopCandidate = new System.Data.SqlClient.SqlCommand
 
+        //separate out data and fill arraylists
+        while (reader.Read())
+        {
+            id = (reader.GetInt32(0));
+            firstName = (reader.GetString(1));
+            lastName = (reader.GetString(2));
+            GPA = (reader.GetDouble(3));
+            studentInterestID = (reader.GetInt32(4));
+
+            topCandidate.Add(new TopCandidate(id, firstName, lastName, GPA, studentInterestID, 0));
+
+
+        }
+        sc.Close();
+
+        ///This was by far the most difficult part, here we are setting the final value equal to
+        ///the each student's GPA + .5 for each interest they have that matches the posting interest. 
+        for (int i = 0; i < topCandidate.Count; i++)
+        {
+            ((TopCandidate)topCandidate[i]).setFinalValue(((TopCandidate)topCandidate[i]).getGPA());
+
+            if (i == 0)
             {
-                ///select the firstname, lastname, GPA, and interest name from all the students who applied to
-                ///a particular job posting
-                Connection = sc,
-                CommandText = "SELECT Student.StudentID, Student.FirstName, Student.LastName, Student.GPA, Student_Interest.interestID " +
-                "FROM Student INNER JOIN Student_Interest ON Student.StudentID = Student_Interest.studentID " +
-                "INNER JOIN Application ON Student.StudentID = Application.studentID INNER JOIN " +
-                "Posting ON Application.postingID = Posting.postingID where Posting.PostingID = @ddlTopCandidate"
-            };
-            getTopCandidate.Parameters.AddWithValue("@ddlTopCandidate", ddlTopCandidate.SelectedValue);
-
-            //create an array list to store the names, GPAs, and interests all separately
-            SqlDataReader reader = getTopCandidate.ExecuteReader();
-
-            int id = 0;
-            String firstName = "";
-            String lastName = "";
-            double GPA = 0;
-            int studentInterestID = 0;
-
-
-
-            //separate out data and fill arraylists
-            while (reader.Read())
-            {
-                id = (reader.GetInt32(0));
-                firstName = (reader.GetString(1));
-                lastName = (reader.GetString(2));
-                GPA = (reader.GetDouble(3));
-                studentInterestID = (reader.GetInt32(4));
-
-                topCandidate.Add(new TopCandidate(id, firstName, lastName, GPA, studentInterestID, 0));
-
-
-
+                for (int j = 0; j < postingInterest.Count; j++)
+                {
+                    if (((TopCandidate)topCandidate[i]).getStudentInterestID().Equals(postingInterest[j]))
+                    {
+                        ((TopCandidate)topCandidate[i]).setFinalValue(((TopCandidate)topCandidate[i]).getGPA() + .3);
+                    }
+                }
             }
-            sc.Close();
 
-            ///This was by far the most difficult part, here we are setting the final value equal to
-            ///the each student's GPA + .5 for each interest they have that matches the posting interest. 
-            for (int i = 0; i < topCandidate.Count; i++)
+            else
             {
-                ((TopCandidate)topCandidate[i]).setFinalValue(((TopCandidate)topCandidate[i]).getGPA());
+                if (((TopCandidate)topCandidate[i]).getFirstName() + ((TopCandidate)topCandidate[i]).getLastName() == ((TopCandidate)topCandidate[i - 1]).getFirstName() + ((TopCandidate)topCandidate[i - 1]).getLastName())
+                {
+                    ((TopCandidate)topCandidate[i]).setFinalValue(((TopCandidate)topCandidate[i - 1]).getFinalValue() + .3);
+                    topCandidate.RemoveAt(i - 1);
+                    i--;
+                }
 
-                if (i == 0)
+                else
                 {
                     for (int j = 0; j < postingInterest.Count; j++)
                     {
@@ -222,58 +246,68 @@ public partial class company_dashboard_StudentContact : System.Web.UI.Page
                         }
                     }
                 }
-
-                else
-                {
-                    if (((TopCandidate)topCandidate[i]).getFirstName() + ((TopCandidate)topCandidate[i]).getLastName() == ((TopCandidate)topCandidate[i - 1]).getFirstName() + ((TopCandidate)topCandidate[i - 1]).getLastName())
-                    {
-                        ((TopCandidate)topCandidate[i]).setFinalValue(((TopCandidate)topCandidate[i - 1]).getFinalValue() + .3);
-                        topCandidate.RemoveAt(i - 1);
-                        i--;
-                    }
-
-                    else
-                    {
-                        for (int j = 0; j < postingInterest.Count; j++)
-                        {
-                            if (((TopCandidate)topCandidate[i]).getStudentInterestID().Equals(postingInterest[j]))
-                            {
-                                ((TopCandidate)topCandidate[i]).setFinalValue(((TopCandidate)topCandidate[i]).getGPA() + .3);
-                            }
-                        }
-                    }
-                }
             }
+        }
 
 
-            ///sort the list of Top Candidates by their final value, and then reverse the list
-            ///so that the top candidates start at postion 0
-            List<TopCandidate> sortedList = topCandidate.OrderBy(o => o.getFinalValue()).ToList();
-            sortedList.Reverse();
+        ///sort the list of Top Candidates by their final value, and then reverse the list
+        ///so that the top candidates start at postion 0
+        List<TopCandidate> sortedList = topCandidate.OrderBy(o => o.getFinalValue()).ToList();
+        sortedList.Reverse();
 
-            ///determine the maximum possible value for a posting
-            double maxFinalValue = 4.0 + (postingInterest.Count * .3);
-
-
-            ////////////////Put data into Graph on Landing Page////////////////
-            ///Grab the Posting Title from the Posting that the top candidates are related to
-            sc.Open();
-            System.Data.SqlClient.SqlCommand getPostingTitle = new System.Data.SqlClient.SqlCommand
-            {
-                Connection = sc,
-                CommandText = "Select PostingTitle from Posting where postingID = @ddlTopCandidate;"
-            };
-            getPostingTitle.Parameters.AddWithValue("@ddlTopCandidate", ddlTopCandidate.SelectedValue);
-
-            String postingSubTitle = getPostingTitle.ExecuteScalar().ToString();
-            getPostingTitle.ExecuteNonQuery();
-            sc.Close();
-
-            ///set the Posting Title in the sub title
-            topCandidateSubTitle.Text = "For: " + postingSubTitle;
+        ///determine the maximum possible value for a posting
+        double maxFinalValue = 4.0 + (postingInterest.Count * .3);
 
 
-            ///Add student information above the progress bar
+        ////////////////Put data into Graph on Landing Page////////////////
+        ///Grab the Posting Title from the Posting that the top candidates are related to
+        sc.Open();
+        System.Data.SqlClient.SqlCommand getPostingTitle = new System.Data.SqlClient.SqlCommand
+        {
+            Connection = sc,
+            CommandText = "Select PostingTitle from Posting where postingID = @ddlTopCandidate;"
+        };
+        getPostingTitle.Parameters.AddWithValue("@ddlTopCandidate", ddlTopCandidate.SelectedValue);
+
+        String postingSubTitle = getPostingTitle.ExecuteScalar().ToString();
+        getPostingTitle.ExecuteNonQuery();
+        sc.Close();
+
+        ///set the Posting Title in the sub title
+        topCandidateSubTitle.Text = "For: " + postingSubTitle;
+
+
+        ///Add student information above the progress bar
+        if (sortedList.Count == 0)
+        {
+            ApplicantOne.Text = "No first candidate in system";
+            ApplicantTwo.Text = "No second candidate in system";
+            ApplicantThree.Text = "No third candidate in system";
+        }
+
+        if (sortedList.Count == 1)
+        {
+            ApplicantOne.Text = sortedList[0].getFirstName() + " " + sortedList[0].getLastName() +
+                "  (Score: " + sortedList[0].getFinalValue() + ")";
+
+            ApplicantTwo.Text = "No second candidate in system";
+            ApplicantThree.Text = "No third candidate in system";
+        }
+
+        if (sortedList.Count == 2)
+        {
+            ApplicantOne.Text = sortedList[0].getFirstName() + " " + sortedList[0].getLastName() +
+                "  (Score: " + sortedList[0].getFinalValue() + ")";
+
+            ApplicantTwo.Text = sortedList[1].getFirstName() + " " + sortedList[1].getLastName() +
+                "  (Score: " + sortedList[1].getFinalValue() + ")";
+
+            ApplicantThree.Text = "No third candidate in system";
+
+        }
+
+        if (sortedList.Count > 2)
+        {
             ApplicantOne.Text = sortedList[0].getFirstName() + " " + sortedList[0].getLastName() +
                 "  (Score: " + sortedList[0].getFinalValue() + ")";
 
@@ -282,43 +316,56 @@ public partial class company_dashboard_StudentContact : System.Web.UI.Page
 
             ApplicantThree.Text = sortedList[2].getFirstName() + " " + sortedList[2].getLastName() +
                 "  (Score: " + sortedList[2].getFinalValue() + ")";
+        }
 
-            ///Display the Data on the chart for the Top Candidate
+        ///Display the Data on the chart for the Top Candidates
+        if (sortedList.Count == 0)
+        {
+            testprogress.Style.Add("width", "0%");
+            testprogress2.Style.Add("width", "0%");
+            testprogress3.Style.Add("width", "0%");
+        }
+
+        if (sortedList.Count == 1)
+        {
             String highestPercentage = (sortedList[0].getFinalValue() / maxFinalValue).ToString(".%");
             testprogress.Style.Add("width", highestPercentage);
 
-            //testprogress.Style.Add("role", "progressbar");
-            //testprogress.Style.Add("aria-valuenow", "75");
-            //testprogress.Style.Add("aria-valuemin", "0");
-            //testprogress.Style.Add("aria-valuemax", "100");
-            //Old HTML: style="width: 75%" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"><
+            testprogress2.Style.Add("width", "0%");
+            testprogress3.Style.Add("width", "0%");
+        }
 
+        if (sortedList.Count == 2)
+        {
+            String highestPercentage = (sortedList[0].getFinalValue() / maxFinalValue).ToString(".%");
+            testprogress.Style.Add("width", highestPercentage);
 
-            ///Display the Data on the chart for the Second-Most Top Candidate
             String secondHighestPercentage = (sortedList[1].getFinalValue() / maxFinalValue).ToString(".%");
             testprogress2.Style.Add("width", secondHighestPercentage);
 
-            //testprogress.Style.Add("role", "progressbar");
-            //testprogress.Style.Add("aria-valuenow", "50");
-            //testprogress.Style.Add("aria-valuemin", "0");
-            //testprogress.Style.Add("aria-valuemax", "100");
-            //Old HTML: style = "width: 50%" role = "progressbar" aria - valuenow = "50" aria - valuemin = "0" aria - valuemax = "100"
+            testprogress3.Style.Add("width", "0%");
+        }
 
+        if (sortedList.Count > 2)
+        {
+            String highestPercentage = (sortedList[0].getFinalValue() / maxFinalValue).ToString(".%");
+            testprogress.Style.Add("width", highestPercentage);
+
+            String secondHighestPercentage = (sortedList[1].getFinalValue() / maxFinalValue).ToString(".%");
+            testprogress2.Style.Add("width", secondHighestPercentage);
 
             String thirdHighestPercentage = (sortedList[2].getFinalValue() / maxFinalValue).ToString(".%");
             testprogress3.Style.Add("width", thirdHighestPercentage);
-
-            //testprogress.Style.Add("role", "progressbar");
-            //testprogress.Style.Add("aria-valuenow", "25");
-            //testprogress.Style.Add("aria-valuemin", "0");
-            //testprogress.Style.Add("aria-valuemax", "100");
-            //Old HTML: style="width: 75%" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"><
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////
         }
-        catch(Exception ex)
-        {
 
-        }
+        ///This stuff is just here incase we need it
+        //testprogress.Style.Add("role", "progressbar");
+        //testprogress.Style.Add("aria-valuenow", "25");
+        //testprogress.Style.Add("aria-valuemin", "0");
+        //testprogress.Style.Add("aria-valuemax", "100");
+        //Old HTML: style="width: 75%" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"><
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     }
 
     /// <summary>
